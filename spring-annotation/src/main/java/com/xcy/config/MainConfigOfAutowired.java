3174aec25c3deab965bda0feb9eb104b32357e14
jerry <13717736@qq.com>
2021-10-19 23:09:57 +0800

    @Autowired 注入两个同样类型的bean，如果找到多个相同类型的组件，再将属性的名称作为组件的id去容器中查找: applicationContext.getBean("userDao")

--- spring-annotation/src/main/java/com/xcy/config/MainConfigOfAutowired.java
+++ spring-annotation/src/main/java/com/xcy/config/MainConfigOfAutowired.java
@@ -11,7 +11,7 @@ import org.springframework.context.annotation.Configuration;
  *
  * 1）、@Autowired:自动注入：
  *      1）、默认优先按照类型去容器中对应的组件：applicationContext.getBean(UserDao.class),找到就赋值。
- *      2)、如果找到多个相同类型的组件，再将属性的名称作为组件的id去容器中查找: applicationContext.getBean("userDao")
+ *      2) 、如果找到多个相同类型的组件，再将属性的名称作为组件的id去容器中查找: applicationContext.getBean("userDao")
  *
  *      UserService{
  *          @Autowired
@@ -25,11 +25,11 @@ import org.springframework.context.annotation.Configuration;
 @ComponentScan({"com.xcy.service", "com.xcy.controller", "com.xcy.dao"})
 public class MainConfigOfAutowired {
 
-    //@Bean("userDao2")
-    //public UserDao userDao() {
-    //    UserDao userDao = new UserDao();
-    //    userDao.setLabel("通过@Bean注解注入的userDao bean, id = userDao2");
-    //    return userDao;
-    //}
+    @Bean("userDao2")
+    public UserDao userDao() {
+        UserDao userDao = new UserDao();
+        userDao.setLabel("通过@Bean注解注入的userDao bean, id = userDao2");
+        return userDao;
+    }
 
 }
